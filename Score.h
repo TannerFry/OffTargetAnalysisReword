79c4ef984da2695a86cd711b2d44142d2a709bb7
TannerFry <tfry2@vols.utk.edu>
2021-09-29 12:52:28 -0500

    updating OT scoring

--- Score.h
+++ Score.h
@@ -10,10 +10,10 @@ class Score
 {
 	public:
 		/**/
-		double shScore(vector<int> &mismatches, vector<string> &hsuKeys, map<string, vector<double >> &hsuMatrix);
+		double shScore(vector<int> &mismatches, vector<string> &hsuKeys, map<string, vector<double >> &hsuMatrix, int &gRNA_length);
 
 		/**/
-		double ssScore(vector<int> &mismatches);
+		double ssScore(vector<int> &mismatches, int &gRNA_length);
 
 		/**/
 		double stScore(vector<int> &mismatches);
